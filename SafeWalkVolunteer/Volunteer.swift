7faca0796ac595d24bf894a6bb2af7db51300d86
nathanrandrews9903 <nathan_andrews@golfdistrict.com>
2024-07-15 10:26:42 -0700

    submission 1 lab exercize 1

--- SafeWalkVolunteer/Volunteer.swift
+++ SafeWalkVolunteer/Volunteer.swift
@@ -12,10 +12,31 @@ protocol Volunteer: ObservableObject {
     var maxHours: Int { get }
 }
 
-// TODO: Create a class called SafeWalkVolunteer that implementes the Volunteer protocol.
+class SafeWalkVolunteer: Volunteer {
+    var name: String
+    var age: Int
+    
 
-// TODO: Create an Int maxHours computed property. It is a get-only property that returns 1 if the volunteer's age is less than 18 and returns 3 if the volunteer's age is 18 and over.
-
-// TODO: Create an initializer without parameters that sets name to an empty string and age to 0.
-
-// TODO: Create another initializer that accepts a a String parameter called name and an Int parameter called age. Assign the parameter values to the corresponding properties of the class.
+    var maxHours: Int {
+        switch age {
+        case 0..<18:
+            return 1
+        case 18...:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    //without parameters
+    init() {
+        self.name = ""
+        self.age = 0
+    }
+    
+    //with parameters
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
